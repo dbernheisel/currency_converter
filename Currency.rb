e761d61db55ef5ed116560af82dd108c3b387fee
David Bernheisel <david@bernheisel.com>
2015-09-03 16:37:52 -0400

    Add string conversion to Currency class

--- Currency.rb
+++ Currency.rb
@@ -5,33 +5,69 @@ class Currency
   attr_accessor :currency
   attr_accessor :amount
 
-  def initialize(amount, currencycode = "USD")
-    @currency = currency
+  # Source http://www.xe.com/symbols.php
+  @@CurrencySymbols = %w|Lek ؋ $ ƒ ман p. BZ$ $b KM P лв R$ ៛ ¥ ₡ kn ₱ Kč kr RD$ £ € ¢ Q L Ft Rp ﷼ ₪ J$ ₩ ₭ Ls Lt ден RM ₨ ₮ MT C$ ₦ B/. Gs S/. zł lei руб Дин. S R CHF NT$ ฿ TT$ ₤ ₴ $U Bs ₫ Z$|
+  @@CurrencyAbbreviations = %w|ALL AFN ARS AWG AUD AZN BSD BBD BYR BZD BMD BOB BAM BWP BGN BRL BND KHR CAD KYD CLP CNY COP CRC HRK CUP CZK DKK DOP XCD EGP SVC EEK EUR FKP FJD GHC GIP GTQ GGP GYD HNL HKD HUF ISK INR IDR IRR IMP ILS JMD JPY JEP KZT KPW KRW KGS LAK LVL LBP LRD LTL MKD MYR MUR MXN MNT MZN NAD NPR ANG NZD NIO NGN NOK OMR PKR PAB PYG PEN PHP PLN QAR RON RUB SHP SAR RSD SCR SGD SBD SOS ZAR LKR SEK CHF SRD SYP TWD THB TTD TRY TRL TVD UAH GBP USD UYU UZS VEF VND YER ZWD|
+
+  def initialize(amount, currencycode = nil)
+    amount = amount.to_s.strip
+    currencycode = @@CurrencySymbols.find { |symbol| amount.include?(symbol) } || @@CurrencyAbbreviations.find { |abb| amount.include?(abb) } if currencycode == nil
+    amount = amount.gsub(/[^\d\.]/, "").to_f
+    @currency = currencycode
     @amount = amount
   end
 
   def to_s
-    "#{currency}#{amount}"
+    "#{sprintf "%.2f" % @amount} #{@currency}"
   end
 
   def ==(money)
-    return false if money.class != Currency
-    (self.currency == money.currency) && (self.amount == money.amount)
+    moneyObj = Currency.new(money) rescue false
+    #puts moneyObj, self, money
+    begin
+      return false if moneyObj.class != Currency
+      self.to_s == moneyObj.to_s
+    rescue
+      return false
+    end
   end
 
   def +(money)
-    raise DifferentCurrencyCodeError, "Cannot add different currencies" unless self.currency == money.currency
-    self.amount + money.amount if self.currency == money.currency
+    begin
+      Float(money)
+    rescue
+      raise DifferentCurrencyCodeError, "Cannot add different currencies" unless self.currency == money.currency
+      Currency.new(self.amount + money.amount, self.currency) if self.currency == money.currency
+    else
+      Currency.new(self.amount + money, self.currency)
+    end
   end
 
   def -(money)
-    raise DifferentCurrencyCodeError, "Cannot subtract different currencies" unless self.currency == money.currency
-    self.amount - money.amount if self.currency == money.currency
+    begin
+      Float(money)
+    rescue
+      raise DifferentCurrencyCodeError, "Cannot subtract different currencies" unless self.currency == money.currency
+      Currency.new(self.amount - money.amount, self.currency) if self.currency == money.currency
+    else
+      Currency.new(self.amount - money, self.currency)
+    end
   end
 
   def *(money)
-    raise DifferentCurrencyCodeError, "Cannot multiply different currencies" unless self.currency == money.currency
-    self.amount * money.amount if self.currency == money.currency
+    begin
+      Float(money)
+    rescue
+      raise DifferentCurrencyCodeError, "Cannot multiply different currencies" unless self.currency == money.currency
+      Currency.new(self.amount * money.amount, self.currency) if self.currency == money.currency
+    else
+      Currency.new(self.amount * money, self.currency)
+    end
   end
 
 end
+
+
+
+
+
--- trade_currency.rb
+++ trade_currency.rb
@@ -4,9 +4,24 @@ require_relative './Currency'
 require_relative './CurrencyConverter'
 require_relative './CurrencyTrader'
 
-davidmoney = Currency.new("$", 100)
-blakemoney = Currency.new("$", 20)
+davidmoney = Currency.new(100, "$")
+blakemoney = Currency.new(100, "$")
 
-puts davidmoney == blakemoney
-puts davidmoney + blakemoney
-puts davidmoney - blakemoney
+puts "======= Tests ========="
+puts "David Money: #{davidmoney}"
+puts "Blake Money: #{blakemoney}"
+puts "To string: #{davidmoney}"
+puts "Equality Currency: #{davidmoney == blakemoney}"
+puts "Equality string: #{davidmoney == "$100"}"
+blakemoney.amount = 20
+puts "Inequality Currency: #{davidmoney == blakemoney}"
+puts "Inequality string: #{davidmoney == "$900"}"
+puts "Addition Currency: #{davidmoney + blakemoney}"
+puts "Addition integer: #{davidmoney + 10}"
+puts "Addition float: #{davidmoney + 10.52}"
+puts "Subtraction: #{davidmoney - blakemoney}"
+puts "Multiplication Currency: #{davidmoney * blakemoney}"
+puts "Multiplication integer: #{davidmoney * 10}"
+puts "Multiplication float: #{davidmoney * 1.5}"
+puts "Equal and Addition Currency: #{davidmoney += blakemoney}, and after #{davidmoney}"
+puts "Equal and Addition Integer: #{davidmoney += 10}, and after #{davidmoney}"
