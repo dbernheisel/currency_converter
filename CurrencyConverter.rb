e912b3ccc43b336f1d0eb6af932595cf580338e1
David Bernheisel <david@bernheisel.com>
2015-09-04 00:36:43 -0400

    Rearchitect currency code find

--- CurrencyConverter.rb
+++ CurrencyConverter.rb
@@ -2,5 +2,40 @@
 
 class CurrencyConverter
 
+  attr_accessor :target_currency
+  attr_accessor :source_currency
 
+  @@rates = { "USD" => {
+                    "USD" => 1.00000,
+                    "EUR" => 0.89932,
+                    "GBP" => 0.65558,
+                    "INR" => 66.0945,
+                    "AUD" => 1.42636,
+                    "CAD" => 1.31838,
+                    "ZAR" => 13.5868,
+                    "NZD" => 1.56566,
+                    "JPY" => 120.085
+                    },
+              "EUR" => {
+                    "USD" => 1.11197,
+                    "EUR" => 1.00000,
+                    "GBP" => 0.72897,
+                    "INR" => 73.4954,
+                    "AUD" => 1.58643,
+                    "CAD" => 1.46597,
+                    "ZAR" => 15.1073,
+                    "NZD" => 1.74092,
+                    "JPY" => 133.538
+                    }
+            }
+
+  def initialize(from, to=nil)
+    @source_currency = from
+  end
+
+  def convert(from, to)
+    initialize()
+    rate = @@rates[from.currency][to]
+    from.amount * rate
+  end
 end
--- trade_currency.rb
+++ trade_currency.rb
@@ -5,13 +5,17 @@ require_relative './CurrencyConverter'
 require_relative './CurrencyTrader'
 
 davidmoney = Currency.new(100, "$")
-blakemoney = Currency.new(100, "$")
-invalidmoney = Currency.new("jfkd200") rescue "Rescued error"
+blakemoney = Currency.new(100, "USD")
+# EXPECTED FAILED TEST invalidmoney = Currency.new("100")
+# FAILED TEST blakemoney3 = Currency.new("$$100")
+invalidmoney2 = Currency.new("jfkd200") rescue "Rescued error"
 
 puts "======= Tests ========="
 puts "David Money: #{davidmoney}"
 puts "Blake Money: #{blakemoney}"
-puts "Invalid Money: #{invalidmoney}"
+#puts "David2 Money: #{davidmoney2}"
+#puts "Blake3 Money: #{blakemoney3}"
+puts "Invalid Money: #{invalidmoney2}"
 puts "To string: #{davidmoney}"
 puts "Equality Currency: #{davidmoney == blakemoney}"
 puts "Equality string: #{davidmoney == "$100"}"
@@ -28,3 +32,6 @@ puts "Multiplication integer: #{davidmoney * 10}"
 puts "Multiplication float: #{davidmoney * 1.5}"
 puts "Equal and Addition Currency: #{davidmoney += blakemoney}, and after #{davidmoney}"
 puts "Equal and Addition Integer: #{davidmoney += 10}, and after #{davidmoney}"
+# converter = CurrencyConverter.new()
+# conversion = converter.convert(davidmoney, "EUR")
+# puts "Convert USD to EUR: #{conversion}"
